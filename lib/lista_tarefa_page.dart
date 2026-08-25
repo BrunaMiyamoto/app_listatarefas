fc972295a550b55e7fa1616f6a63dc536b022fcb
bruna miyamoto <brumiyamoto00@gmail.com>
2026-08-25 14:46:24 -0300

    Criação de carde de tarefa pendente e botão

--- lib/lista_tarefa_page.dart
+++ lib/lista_tarefa_page.dart
@@ -11,6 +11,8 @@ class ListaTarefaPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: ListView(
+        //pense nele como um container
+        padding: EdgeInsets.all(12),
         children: [
           // for (var i = 1; i <= 50; i++) ...[
           //   //para criar u,a estrutura de repetição FOR se precisar
@@ -22,11 +24,61 @@ class ListaTarefaPage extends StatelessWidget {
                 Icons.check_circle,
                 color: Colors.green,
               ),
-              title: Text("Configurar o ambiente de desenvolvimento"),
-              subtitle: Text("Concluida."),
+              title: Text(
+                "Configurar o ambiente de desenvolvimento",
+                style: TextStyle(decoration: TextDecoration.lineThrough),
+              ),
+              subtitle: Text("Concluida"),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.circle_outlined,
+                color: Colors.grey,
+              ),
+              title: Text(
+                "Fazer a atividade Flutter",
+              ),
+              subtitle: Text("Pendente"),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.check_circle,
+                color: Colors.green,
+              ),
+              title: Text(
+                "Iniciar layout Mobile",
+                style: TextStyle(decoration: TextDecoration.lineThrough),
+              ),
+              subtitle: Text("Concluida"),
+              trailing: Icon(
+                Icons.delete_outline,
+                color: Colors.grey,
+              ),
             ),
           ),
         ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+        //shape: CircleBorder(), deixa o botão arredondado
+        child: Icon(Icons.add),
       ),
     );
   }
