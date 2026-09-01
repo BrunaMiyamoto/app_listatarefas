1262e9fda30f159e7c6a3ccfd3064c304f5e8c06
bruna miyamoto <brumiyamoto00@gmail.com>
2026-09-01 15:26:43 -0300

    Implementação da lista em variável e listviewBiulder - Fase B

--- lib/lista_tarefa_page.dart
+++ lib/lista_tarefa_page.dart
@@ -5,72 +5,60 @@ class ListaTarefaPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<
+      Map<String, dynamic>
+    > //variavel do tipo list pode ser associada a um array simples, dentro dos sinais de maior e menos colocamos o tipo de dado que irá entrar, caso seja variado podemos deixar sem nada. Nesse caso a chave será string e o valor será dinâmico
+    tarefas = [
+      {'titulo': 'Fazer compras', 'situacao': false},
+      {'titulo': 'Lavar roupas', 'situacao': true},
+      {'titulo': 'Pagar contas', 'situacao': false},
+      {'titulo': 'Fazer tarefa', 'situacao': true},
+      {"titulo": 'Limpar casa', 'situacao': false},
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas Tarefas"),
         centerTitle: true,
       ),
-      body: ListView(
-        //pense nele como um container
+      body: ListView.builder(
+        //pense nele como um container, listview sozinho trabalha com dados estáticos
         padding: EdgeInsets.all(12),
-        children: [
+        itemCount: tarefas
+            .length, //define a quantidade de elementos associadas a lista que criamos
+        itemBuilder: (context, index) {
+          // children: [// esse elemento não existe em listview.builder, é substituido por itemBuilder
           // for (var i = 1; i <= 50; i++) ...[
           //   //para criar u,a estrutura de repetição FOR se precisar
           //   Text("Item 1"),
           // ],
-          Card(
-            child: ListTile(
-              leading: Icon(
-                Icons.check_circle,
-                color: Colors.green,
-              ),
-              title: Text(
-                "Configurar o ambiente de desenvolvimento",
-                style: TextStyle(decoration: TextDecoration.lineThrough),
-              ),
-              subtitle: Text("Concluida"),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
-            ),
-          ),
 
-          Card(
-            child: ListTile(
-              leading: Icon(
-                Icons.circle_outlined,
-                color: Colors.grey,
-              ),
-              title: Text(
-                "Fazer a atividade Flutter",
-              ),
-              subtitle: Text("Pendente"),
-              trailing: Icon(
-                Icons.delete_outline,
-                color: Colors.grey,
-              ),
-            ),
-          ),
+          final tarefa =
+              tarefas[index]; //criamos uma variavel para receber cada uma das tarefas da lista
 
-          Card(
+          final bool situacao = tarefa["situacao"];
+
+          return Card(
             child: ListTile(
               leading: Icon(
-                Icons.check_circle,
-                color: Colors.green,
+                situacao ? Icons.check_circle : Icons.circle_outlined,
+                color: situacao ? Colors.green : Colors.grey,
               ),
               title: Text(
-                "Iniciar layout Mobile",
-                style: TextStyle(decoration: TextDecoration.lineThrough),
+                tarefa["titulo"],
+                style: TextStyle(
+                  decoration: situacao
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
               ),
-              subtitle: Text("Concluida"),
+              subtitle: Text(situacao ? "Concluida" : "Pendente"),
               trailing: Icon(
                 Icons.delete_outline,
                 color: Colors.grey,
               ),
             ),
-          ),
-        ],
+          );
+        },
       ),
 
       floatingActionButton: FloatingActionButton(
